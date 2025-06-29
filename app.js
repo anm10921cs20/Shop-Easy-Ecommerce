5f2a1a046e7f8664ed8057c631e35b4d9fc0081c
mahavishnu <mahanvishnu3@gamil.com>
2025-06-29 12:04:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//app nav bars container
+//display change method
+
+//display function event
+
+var openBar = document.getElementById('app-nav-bars');
+var openBtn = document.getElementById('app-bars');
+
+//event listener nav open
+openBtn.addEventListener('click', () => {
+    openBar.style.marginLeft = "0px";
+    document.body.style.opacity = "0.9";
+})
+
+//event listener nav close
+
+var closeBtn = document.getElementById('nav-close');
+closeBtn.addEventListener('click', () =>
+{
+    openBar.style.marginLeft = "-1000px";
+    openBar.style.transitionDuration = "0.9s";
+})
+
+
+
+
