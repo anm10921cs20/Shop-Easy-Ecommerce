edcd06de7334f591d0d0b7309e0b4e4c1f514470
mahavishnu <mahanvishnu3@gamil.com>
2025-07-14 22:13:40 +0530

    update 17

---
+++ public/fashion/app.js
@@ -0,0 +1,25 @@
+//app nav bars container
+//display change method
+
+//display function event
+
+var openBtn = document.getElementById('app-arrow');
+
+//event listener nav open
+openBtn.addEventListener('click', () => {
+   window.history.back();
+})
+
+//event listener nav close
+
+
+
+
+
+
+
+
+
+
+
+
