6a8290cf10dc76a79b4f8ecc2083306664174c9e
mahavishnu <mahanvishnu3@gamil.com>
2025-08-14 23:53:08 +0530

    update 37

---
+++ public/mobile/products brands/realme/app.js
@@ -0,0 +1,53 @@
+//app nav bars container
+//display change method
+
+//display function event
+
+
+
+var openBtn = document.getElementById('app-arrow');
+
+//event listener nav open
+openBtn.addEventListener('click', () => {
+   window.history.back();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function
+
+function itemsremove()
+{
+    allItems.style.display="none";
+}
+
+
+
+
+
+
+
+//cart
+
+
+
+
+
