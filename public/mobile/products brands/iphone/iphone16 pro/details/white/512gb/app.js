1d77edf73dcb5e77aa1da98a974942e1b4a98b67
mahavishnu <mahanvishnu3@gamil.com>
2025-08-17 03:30:53 +0530

    update 39

--- public/mobile/products brands/iphone/iphone16 pro/details/white/512gb/app.js
+++ public/mobile/products brands/iphone/iphone16 pro/details/white/512gb/app.js
@@ -11,6 +11,54 @@ openBtn.addEventListener('click', () => {
 })
 
 
+// heart btn
+
+
+//special btns added
+
+document.addEventListener('DOMContentLoaded', loadcontent)
+
+
+
+function loadcontent()
+{
+    const button = document.querySelectorAll('.btn-main');
+    button.forEach((btn) => {
+        btn.addEventListener('click',selected);
+    })
+
+    // heart btn
+
+    const hearts = document.querySelectorAll('.buttons');
+    hearts.forEach((heart) => {
+        heart.addEventListener('click',heartactive)
+    })
+
+
+
+
+}
+
+
+
+//content remove
+function selected()
+{
+    this.classList.toggle('btn-main-active');
+
+    
+}
+
+
+//heart btn added and remove
+
+function heartactive()
+{
+    this.classList.toggle('buttons-active');
+}
+
+
+
 // page vidio open
 
 const vidoplay = document.getElementById('video-play');
@@ -28,4 +76,45 @@ videoBlur.addEventListener('click', () => {
     videoBlur.classList.remove('blur-container-active');
     const videocontainer = document.querySelector('.video-container');
     videocontainer.style.display = "none";
+})
+
+
+
+// know more container
+
+const knowmore = document.getElementById('know-more');
+const knowmoreContainer = document.querySelector('.know-more-container');
+const knowmoreClose = document.getElementById('know-more-close');
+
+knowmore.addEventListener('click', () => {
+    knowmoreContainer.style.display = "block";
+})
+knowmoreClose .addEventListener('click', () => {
+    knowmoreContainer.style.display = "none";
+})
+
+
+
+// products details container
+
+const productshigh = document.getElementById('proddetails');
+const productschev = document.getElementById('chevrons');
+const productsvalues = document.getElementById('full-details')
+
+productshigh.addEventListener('click', () => {
+    productschev.classList.toggle('down-hover');
+    productsvalues.classList.toggle('full-details-active');
+
+})
+
+
+// image change function
+
+const imageChange = document.querySelector('#imagechange');
+
+imageChange.addEventListener('click', () => {
+    imageChange.src = "https://rukminim1.flixcart.com/www/120/102/promos/04/09/2023/d8a65dd0-321c-4028-9496-660e67ad6696.png?q=60";
+    const a = document.createElement('a');
+    a.href = "../../../../../../../insidecart/insidecart.html"
+ 
 })
