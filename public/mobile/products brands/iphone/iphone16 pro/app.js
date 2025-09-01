4cb5bd3f65d054f8836cc1e8b2de114d2abc5b5c
mahavishnu <mahanvishnu3@gamil.com>
2025-09-01 22:32:56 +0530

    update 47.1

--- public/mobile/products brands/iphone/iphone16 pro/app.js
+++ public/mobile/products brands/iphone/iphone16 pro/app.js
@@ -100,7 +100,7 @@ btnlaunched.addEventListener('click', () => {
 window.onload = function()
 {
     const cartcount = localStorage.getItem('cart-count')
-    const cartbox  = document.getElementById('cart-counts')
-    cartbox.innerText = cartcount;
+    const cartbox  = document.getElementById('cart-counts');
+    cartbox.innerText += cartcount;
 }
 
