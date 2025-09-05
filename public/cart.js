81f0b876751a4917ed9b9b9a41968b4d4f4bad67
Mahavishnu <145285335+anm10921cs20@users.noreply.github.com>
2025-09-06 00:02:03 +0530

    Update cart.js

--- public/cart.js
+++ public/cart.js
@@ -17,54 +17,6 @@ for (let i = 0; i < cartheart.length; i++) {
     heart.addEventListener('click', heartclicked)
 }
 
-// add items to heart like
-
-function heartclicked(event) {
-    var heart = event.target;
-    var heartitemsvalue = heart.parentElement;
-
-
-
-
-    var hearts = [];
-
-    var heartdetails = {
-        anchor: heartitemsvalue.parentElement.href,
-        img: heartitemsvalue.getElementsByClassName('product-img')[0].src,
-        brand: heartitemsvalue.getElementsByClassName('mobile-brand-name')[0].innerText,
-        name: heartitemsvalue.getElementsByClassName('mobile-name')[0].innerText,
-        price: heartitemsvalue.getElementsByClassName('rate')[0].innerText,
-        delprice: heartitemsvalue.getElementsByClassName('rate-del')[0].innerText,
-        percent: heartitemsvalue.getElementsByClassName('percentage')[0].innerText,
-        bank: heartitemsvalue.getElementsByClassName('bank-details')[0].innerText,
-        warrantybrand: heartitemsvalue.getElementsByClassName('waranty')[0].innerText,
-        no: 1
-    }
-
-
-    if (JSON.parse(localStorage.getItem('heart')) === null) {
-        hearts.push(heartdetails);
-        const heartcontainer = JSON.stringify(hearts);
-        localStorage.setItem('heart', heartcontainer);
-    }
-    else {
-        const localhearts = JSON.parse(localStorage.getItem('heart'));
-        localhearts.map(heartdata => {
-            if (heartdetails.brand == heartdata.brand) {
-                heartdetails.no = heartdetails.no + 1;
-            }
-            else {
-                hearts.push(heartdata);
-            }
-        });
-        hearts.push(heartdetails);
-        localStorage.setItem('heart', JSON.stringify(hearts))
-    }
-
-}
-
-
-
 //add items cart infucntion
 
 function additemsclicked(event) {
@@ -142,6 +94,59 @@ function additemsclicked(event) {
 
   
 
+
+
+
+
+
+
+
+
+// add items to heart like
+
+function heartclicked(event) {
+    var heart = event.target;
+    var heartitemsvalue = heart.parentElement;
+
+
+
+
+    var hearts = [];
+
+    var heartdetails = {
+        anchor: heartitemsvalue.parentElement.href,
+        img: heartitemsvalue.getElementsByClassName('product-img')[0].src,
+        brand: heartitemsvalue.getElementsByClassName('mobile-brand-name')[0].innerText,
+        name: heartitemsvalue.getElementsByClassName('mobile-name')[0].innerText,
+        price: heartitemsvalue.getElementsByClassName('rate')[0].innerText,
+        delprice: heartitemsvalue.getElementsByClassName('rate-del')[0].innerText,
+        percent: heartitemsvalue.getElementsByClassName('percentage')[0].innerText,
+        bank: heartitemsvalue.getElementsByClassName('bank-details')[0].innerText,
+        warrantybrand: heartitemsvalue.getElementsByClassName('waranty')[0].innerText,
+        no: 1
+    }
+
+
+    if (JSON.parse(localStorage.getItem('heart')) === null) {
+        hearts.push(heartdetails);
+        const heartcontainer = JSON.stringify(hearts);
+        localStorage.setItem('heart', heartcontainer);
+    }
+    else {
+        const localhearts = JSON.parse(localStorage.getItem('heart'));
+        localhearts.map(heartdata => {
+            if (heartdetails.brand == heartdata.brand) {
+                heartdetails.no = heartdetails.no + 1;
+            }
+            else {
+                hearts.push(heartdata);
+            }
+        });
+        hearts.push(heartdetails);
+        localStorage.setItem('heart', JSON.stringify(hearts))
+    }
+
+}
 
 
 
