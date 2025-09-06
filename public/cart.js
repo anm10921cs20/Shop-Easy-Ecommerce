a4e3852b33f25e6d8900391a2a85fdf200714d9e
mahavishnu <mahanvishnu3@gamil.com>
2025-09-06 14:55:10 +0530

    update 51.1

--- public/cart.js
+++ public/cart.js
@@ -12,7 +12,6 @@ for (let i = 0; i < cartheart.length; i++) {
     heart.addEventListener('click', heartclicked)
 }
 
-<<<<<<< HEAD
 // add items to heart like
 
 function heartclicked(event) {
@@ -62,9 +61,6 @@ function heartclicked(event) {
 
 
 
-=======
-//add items cart infucntion
->>>>>>> 81f0b876751a4917ed9b9b9a41968b4d4f4bad67
 
 
 
@@ -84,64 +80,6 @@ function heartclicked(event) {
 
 
 
-<<<<<<< HEAD
-=======
-// add items to heart like
-
-function heartclicked(event) {
-    var heart = event.target;
-    var heartitemsvalue = heart.parentElement;
-
-
-
-
-    var hearts = [];
-
-    var heartdetails = {
-        anchor: heartitemsvalue.parentElement.href,
-        img: heartitemsvalue.getElementsByClassName('product-img')[0].src,
-        brand: heartitemsvalue.getElementsByClassName('mobile-brand-name')[0].innerText,
-        name: heartitemsvalue.getElementsByClassName('mobile-name')[0].innerText,
-        price: heartitemsvalue.getElementsByClassName('rate')[0].innerText,
-        delprice: heartitemsvalue.getElementsByClassName('rate-del')[0].innerText,
-        percent: heartitemsvalue.getElementsByClassName('percentage')[0].innerText,
-        bank: heartitemsvalue.getElementsByClassName('bank-details')[0].innerText,
-        warrantybrand: heartitemsvalue.getElementsByClassName('waranty')[0].innerText,
-        no: 1
-    }
-
-
-    if (JSON.parse(localStorage.getItem('heart')) === null) {
-        hearts.push(heartdetails);
-        const heartcontainer = JSON.stringify(hearts);
-        localStorage.setItem('heart', heartcontainer);
-    }
-    else {
-        const localhearts = JSON.parse(localStorage.getItem('heart'));
-        localhearts.map(heartdata => {
-            if (heartdetails.brand == heartdata.brand) {
-                heartdetails.no = heartdetails.no + 1;
-            }
-            else {
-                hearts.push(heartdata);
-            }
-        });
-        hearts.push(heartdetails);
-        localStorage.setItem('heart', JSON.stringify(hearts))
-    }
-
-}
-
-
-
-
-
-
-
-var counts = document.getElementById('cart-count');
-var countvalue = JSON.parse(localStorage.getItem('order')).length;
-counts.innerText = countvalue;
->>>>>>> 81f0b876751a4917ed9b9b9a41968b4d4f4bad67
 
 
 
