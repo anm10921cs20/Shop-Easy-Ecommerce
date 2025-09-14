5349104f6eb301ed97c646cfdd903742e3074d10
mahavishnu <mahanvishnu3@gamil.com>
2025-09-14 23:15:58 +0530

    update 60.3

--- public/myorder/app.js
+++ public/myorder/app.js
@@ -25,7 +25,7 @@ db.ref('maincart/' + localStorage.getItem('userid') + localStorage.getItem('name
     .get('value')
     .then((snapshot) => {
         const order = snapshot.val();
-        console.log(order);
+
 
         const maindata = Object.entries(order);
 
@@ -33,7 +33,7 @@ db.ref('maincart/' + localStorage.getItem('userid') + localStorage.getItem('name
         maindata.forEach((data) => {
 
             const vl = data[1]
-            console.log(data[1].address);
+
 
 
             var orders = vl.order;
@@ -45,7 +45,22 @@ db.ref('maincart/' + localStorage.getItem('userid') + localStorage.getItem('name
 
 
             const datass = JSON.parse(add[2])
-            console.log(datass);
+            const twoDays = 2 * 24 * 60 * 60 * 1000;
+            const datereg = data[1].date;
+
+            
+            const delivery = document.getElementsByClassName('delivery');
+            if (datereg >= twoDays) {
+
+                for (var d = 0; d < delivery.length; d++) {
+                    delivery[d].style.color = "var(--bs-success)";
+                    delivery[d].innerText = "Deliverd Success";
+                }
+            } else {
+                console.log('not going delivery');
+
+
+            }
 
             for (var i = 0; i < datass.length; i++) {
                 const div = document.createElement('div')
@@ -56,31 +71,101 @@ db.ref('maincart/' + localStorage.getItem('userid') + localStorage.getItem('name
                 const contentcontainer = document.getElementsByClassName('orders-items')[0];
                 contentcontainer.appendChild(maindataContainer)
                 const seconddiv = document.createElement('div');
-                seconddiv.innerHTML = ` <p>2days Delivery</p>
+                seconddiv.classList.add('seconddiv')
+                seconddiv.innerHTML = ` <p class="delivery">2days Delivery</p>
                 <p class="deliver-name">${datass[i].name}</p>
+                <p class="time"> Order on ${data[1].date}</p>
+
 
             `;
-            maindataContainer.appendChild(seconddiv)
-            const thirddiv = document.createElement('div');
-            thirddiv.classList.add('chevron-div')
-            thirddiv.innerHTML = `<i class="fa-solid fa-chevron-right"></i>
+                maindataContainer.appendChild(seconddiv)
+                const thirddiv = document.createElement('div');
+                thirddiv.classList.add('chevron-div')
+                thirddiv.innerHTML = `<i class="fa-solid fa-chevron-right"></i>
             `
-            maindataContainer.appendChild(thirddiv)
+                maindataContainer.appendChild(thirddiv)
 
-           
-            if(contentcontainer.children.length < 0)
-            {
-                const emptyDiv = document.createElement('div')
-                emptyDiv.classList.add('emptydiv');
-                emptyDiv.innerHTML = `
-                <p class=""empty-para>Orders Is Empty Please Order AnyThings</p>
+
+
+
+                // details containers
+
+
+                const maindetailscontainer = document.createElement('div');
+                maindetailscontainer.classList.add('maindetailscontainer');
+                const detailsitems = document.getElementsByClassName('details-items')[0];
+                detailsitems.append(maindetailscontainer);
+
+                // nav bar
+
+                const navheader = document.createElement('div');
+                navheader.classList.add('nav-bars');
+                navheader.innerHTML = `
+                 <div class="cart-header">
+            <nav class="nav-bar">
+                <div class="app-left-bar">
+                    <i id="app-arrow" class="fas fa-arrow-left app-bars app-arc"></i>
+                </div>
+                <div class="app-title">
+                    <h4 class="app-title_head">Order Details</h4>
+                </div>
+            </nav>
+        </div><hr>`
+                maindetailscontainer.appendChild(navheader)
+
+                // main data
+
+                const maindata = document.createElement('div');
+                maindata.classList.add('maindata');
+                maindata.innerHTML = `<div class="img-container"><img src="${datass[i].img}" class="img-det"></div>
+                <div class="name-container">${datass[i].name}<p>(${datass[i].brand})</p></div><br>
                 `;
-                contentcontainer.appendChild(emptyDiv);
-            }
-            
+
+                maindetailscontainer.appendChild(maindata)
+
+                // color div
+
+                const colordiv = document.createElement('div');
+                colordiv.classList.add('color-area');
+                maindetailscontainer.appendChild(colordiv)
+                // order summary
+
+                const ordersummary = document.createElement('div');
+                ordersummary.classList.add('order-summary');
+                ordersummary.innerHTML = `<br>
+                <p class="ordersummary-tit">Order Summary</p>
+                <div class="ordersummary-data">
+                <p class="tit">Name</p><p class="ans">${datass[i].name}</p>
+                <p class="tit">Brand Varient</p><p class="ans">${datass[i].brand}</p>
+                <p class="tit">Price</p><p class="ans">${datass[i].price}</p>
+                <p class="tit">Order Time</p><p class="ans">${data[1].date}</p>
+                <p class="tit">Order Id</p><p class="ans">${data[0]}</p>
+                </div><br>
+                `;
+                maindetailscontainer.appendChild(ordersummary)
+                const colordiv1 = document.createElement('div');
+                colordiv1.classList.add('color-area');
+                maindetailscontainer.appendChild(colordiv1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             }
 
-            
+
 
 
 
@@ -89,6 +174,27 @@ db.ref('maincart/' + localStorage.getItem('userid') + localStorage.getItem('name
 
 
         })
+        var dataContainer = document.getElementsByClassName('first-order');
+
+
+        // app close
+        for (let j = 0; j < dataContainer.length; j++) {
+            var btn = dataContainer[j]
+            btn.addEventListener('click', () => {
+                const maincontainr = document.getElementsByClassName('maindetailscontainer');
+                maincontainr[j].style.display = "block";
+            })
+            var btnClose = document.querySelectorAll('#app-arrow');
+            btnClose.forEach((btncloses) => {
+                btncloses.addEventListener('click', () => {
+                    const maincontainr1 = document.getElementsByClassName('maindetailscontainer');
+                    maincontainr1[j].style.display = "none";
+                })
+            })
+
+        }
+
+
 
     })
 
@@ -96,9 +202,31 @@ db.ref('maincart/' + localStorage.getItem('userid') + localStorage.getItem('name
 
 
 
-    
-    
 
 
+
+
+
+
+
+
+
+function clicked() {
+
+
+
+}
+
+// close
+
+
+
+const maincontainr = document.getElementsByClassName('maindetailscontainer');
+
+for (var i = 0; i < maincontainr.length; i++) {
+    var appclose = document.getElementsByClassName('app-bars')[i];
+    console.log(appclose);
+
+}
 
 
